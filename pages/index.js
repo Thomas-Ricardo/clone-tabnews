212cfd775a44ff51984f540d7877c3101c407b06
Thomas-Ricardo <thomasricardo1000@hotmail.com>
2025-10-26 11:35:06 -0300

    First commit

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1 style={{ fontFamily: "sans-serif", fontSize: "21px", width: "100%", height: "100vh", display:"flex", justifyContent:"center", alignItems: "center", textAlign: "center", padding: "30px", }}>Eu Te Amo Najla 😍💗</h1>;
+  return <h1 style={{ fontFamily: "sans-serif", fontSize: "21px", width: "100%", height: "100vh", display:"flex", justifyContent:"center", alignItems: "center", textAlign: "center", padding: "30px", }}>Teste msg.</h1>;
 }
 
 export default Home;
